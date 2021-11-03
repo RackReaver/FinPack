4bb03bde21ca4a6a7ca3a6aed875f890cfaa76d2
rackreaver <rackreaver@gmail.com>
2021-11-03 15:35:36 -0400

    feat: :sparkles: Add _add_char utility function

---
+++ finpack/utils.py
@@ -0,0 +1,12 @@
+def _add_char(num, char=' '):
+    """Creates a string value give a number and character.
+    args:
+        num (int): Amount to repeat character
+    kwargs:
+        char (str): Character value to lopp
+    Returns (str): Iterated string value for given character
+    """
+    string = ''
+    for i in range(num):
+        string += char
+    return string
