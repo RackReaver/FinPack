825bcdb75f08d28dad40d7085c439fb3bcd63017
rackreaver <rackreaver@gmail.com>
2021-11-09 21:27:16 -0500

    style: :art: 'black' auto reformatter

--- finpack/utils.py
+++ finpack/utils.py
@@ -1,12 +1,12 @@
-def _add_char(num, char=' '):
+def _add_char(num, char=" "):
     """Creates a string value give a number and character.
     args:
         num (int): Amount to repeat character
     kwargs:
-        char (str): Character value to lopp
+        char (str): Character value to loop
     Returns (str): Iterated string value for given character
     """
-    string = ''
+    string = ""
     for i in range(num):
         string += char
     return string
