8d501492db75e913a9640788274862990b1a74be
rackreaver <rackreaver@gmail.com>
2021-11-09 21:25:05 -0500

    style: :art: 'black' auto reformatting

--- finpack/core.py
+++ finpack/core.py
@@ -8,15 +8,20 @@ from datetime import datetime
 
 from finpack import utils
 
+
 class DataError(Exception):
     """Data not provided or available as expected."""
+
     pass
+
 
 class AccountError(Exception):
     """Account class exception"""
+
     pass
 
-class Account():
+
+class Account:
     def __init__(self, name, type, category, sub_category, description, history):
         self.name = name
         self.short_name = name[:40]
@@ -25,19 +30,18 @@ class Account():
         self.sub_category = sub_category
         self.description = description
         self.history = history
-        
+
         if len(self.name) > 40:
-            self.short_name = name[:37] + '...'
+            self.short_name = name[:37] + "..."
 
     def __repr__(self):
-        return '<Account.' + self.type + '.' + self.name.replace(' ', '-') + '>'
+        return "<Account." + self.type + "." + self.name.replace(" ", "-") + ">"
 
     def __eq__(self, other):
-        return ' '.join([self.type, self.name]) == other
+        return " ".join([self.type, self.name]) == other
 
     def current_value(self):
-        """Get latest monetary value of account.
-        """
+        """Get latest monetary value of account."""
         first = True
         for val in self.history:
             if first == True:
@@ -48,10 +52,10 @@ class Account():
                     value = val
 
         return value[1]
-    
+
     def add_value(self, value, date=datetime.now()):
         """
-        
+
         args:
             value (int|float): Account value
 
@@ -66,59 +70,71 @@ class Account():
             if type(value) == int:
                 value = float(value)
             else:
-                raise DataError('Wrong variable type passed to function, "value" should be a float or int not {}'.format(type(value).__name__))
+                raise DataError(
+                    'Wrong variable type passed to function, "value" should be a float or int not {}'.format(
+                        type(value).__name__
+                    )
+                )
         if not isinstance(date, datetime):
-            raise DataError('Wrong variable type passed to function, "date" should be a datetime not {}'.format(type(date).__name__))
+            raise DataError(
+                'Wrong variable type passed to function, "date" should be a datetime not {}'.format(
+                    type(date).__name__
+                )
+            )
 
         # Verify date value does not exist
         if date not in [x[0] for x in self.history]:
             self.history.append([date, "{:,.2f}".format(value)])
         else:
             # TODO: Prompt to overwrite and allow for auto overwrite.
-            raise AccountError('Date value already exists')
-
+            raise AccountError("Date value already exists")
 
 
 def importer(filepath, header=True):
     """Import chart of accounts from CSV file.
-    
+
     args:
         filepath (str): Location of CSV file.
     kwargs:
         header (bool): If column names are included in file.
 
-    return (dict): 
+    return (dict):
     """
     accounts = []
 
-    with open(filepath, 'r') as openFile:
+    with open(filepath, "r") as openFile:
         r = csv.DictReader(openFile)
         # Loop through all rows
         for row in r:
 
             # Check if account name already exists
-            if ' '.join([row['type'], row['name']]) in accounts:
-                raise DataError('Account names must be unique')
+            if " ".join([row["type"], row["name"]]) in accounts:
+                raise DataError("Account names must be unique")
 
-            ignore = ['name', 'type', 'category', 'sub_category', 'description']
+            ignore = ["name", "type", "category", "sub_category", "description"]
             # Parse out only financial data
-            data = [[x[0], x[1].replace(',', '')] for x in row.items() if x[0] not in ignore]
+            data = [
+                [x[0], x[1].replace(",", "")] for x in row.items() if x[0] not in ignore
+            ]
 
             # Add account to accounts list
-            accounts.append(Account(
-                                name=row['name'],
-                                type=row['type'],
-                                category=row['category'],
-                                sub_category=row['sub_category'],
-                                description=row['description'],
-                                history=data
-                                ))
+            accounts.append(
+                Account(
+                    name=row["name"],
+                    type=row["type"],
+                    category=row["category"],
+                    sub_category=row["sub_category"],
+                    description=row["description"],
+                    history=data,
+                )
+            )
 
     return accounts
 
+
 def balsheet(data, date):
     """Build balance sheet
-    
+
     args:
         data (list): data from importer
         date (datetime): date in datetime format
@@ -126,12 +142,14 @@ def balsheet(data, date):
     # TODO: Build Balance Sheet Report Function
     pass
 
+
 def cashflow(data):
     """Build cashflow statement"""
     # TODO: Build Cash Flow Report Function
     pass
 
+
 def allocation(data):
     """Build asset and liability allocation"""
     # TODO: Build Allocation Report Function
-    pass    
+    pass
