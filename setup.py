a3774ac8a6ab3a94cdac2b3bc052c39b3996682f
rackreaver <rackreaver@gmail.com>
2021-11-22 14:03:07 -0500

    style: :art: Auto formatter

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 """Python module/package configuration file.
 """
 __copyright__ = "Copyright (C) 2020  Matt Ferreira"
@@ -10,24 +9,25 @@ from os import path
 from setuptools import find_packages, setup
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     README = f.read()
 
-with open('finpack/VERSION') as version_file:
+with open("finpack/VERSION") as version_file:
     version = version_file.read().strip()
 assert isinstance(version, str)
 
 install_requirements = []
 
-setup(name='finpack',
-      version=version,
-      py_modules=['finpack'],
-      description='Super simple financial tracking and management tools.',
-      long_description=README,
-      license='Apache License',
-      author='Matt Ferreira',
-      author_email='rackreaver@gmail.com',
-      download_url='https://github.com/RackReaver/FinPack',
-      install_requires=install_requirements,
-      packages=find_packages()
-      )
+setup(
+    name="finpack",
+    version=version,
+    py_modules=["finpack"],
+    description="Super simple financial tracking and management tools.",
+    long_description=README,
+    license="Apache License",
+    author="Matt Ferreira",
+    author_email="rackreaver@gmail.com",
+    download_url="https://github.com/RackReaver/FinPack",
+    install_requires=install_requirements,
+    packages=find_packages(),
+)
