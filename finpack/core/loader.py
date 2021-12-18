bb204f026934e971d81b8809d1393bf38e064fcf
Matt Ferreira <rackreaver@gmail.com>
2021-12-17 21:01:21 -0500

    refactor: :recycle: Improve readability, remove bloat and added basic verbose logging. (#51)

--- finpack/core/loader.py
+++ finpack/core/loader.py
@@ -3,33 +3,29 @@
 __copyright__ = "Copyright (C) 2021 Matt Ferreira"
 
 import csv
+import logging
 
 from finpack.core.exceptions import DataError
 from finpack.core.models import Account
 
 
-def loader(filepath, header=True):
+def loader(filepath):
     """Import chart of accounts from CSV file.
 
     args:
         filepath (str): Location of CSV file.
-    kwargs:
-        header (bool): If column names are included in file.
 
-    return (dict):
+    return (dict): Account classes
     """
+    logging.info("Running finpack.core.loader:loader")
     accounts = []
 
     with open(filepath, "r") as openFile:
-        r = csv.DictReader(openFile)
-        # Loop through all rows
-        for row in r:
-
-            # Skip row if name and type are blank
-            if row["name"] == "" and row["type"] == "":
+        logging.debug("Looping through rows in CSV file.")
+        for num, row in enumerate(csv.DictReader(openFile)):
+            if row["name"] == "" or row["type"] == "":
+                logging.debug(f"Skipping row {num} because name or type was blank.")
                 break
-
-            # Check if account name already exists
             if " ".join([row["type"], row["name"]]) in accounts:
                 raise DataError(
                     "Account names must be unique if same type, '{}' is duplicated.".format(
@@ -38,10 +34,10 @@ def loader(filepath, header=True):
                 )
 
             ignore = ["name", "type", "category", "sub_category", "description"]
-            # Parse out only financial data
-            data = [
-                [x[0], x[1].replace(",", "")] for x in row.items() if x[0] not in ignore
-            ]
+            data = []
+            for x in row.items():
+                if x[0] not in ignore and x[1].replace(" ", "") is not "":
+                    data.append([x[0], x[1].replace(",", "")])
 
             # Add account to accounts list
             accounts.append(
@@ -54,5 +50,5 @@ def loader(filepath, header=True):
                     history=data,
                 )
             )
-
+    logging.info("Completed running finpack.core.loader:loader")
     return accounts
