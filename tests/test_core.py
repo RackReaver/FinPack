6492fe101cc0fd65dd9c39750fab122dfd294127
RackReaver <rackreaver@gmail.com>
2021-12-23 10:45:22 -0500

    fix: :white_check_mark: Fix broken account.value test
    Fixed broken test caused by a change in functions return value.

--- tests/test_core.py
+++ tests/test_core.py
@@ -89,7 +89,7 @@ class TestCore(unittest.TestCase):
         self.assertTrue(self.account == TYPE.lower() + " " + NAME)
 
     def test_value(self):
-        self.assertEqual(self.account.value(datetime(2021, 12, 1)), "2000.00")
+        self.assertEqual(self.account.value(datetime(2021, 12, 1)), 2000.00)
 
     def test_add_value(self):
         temp = self.account.add_value(3000, date=datetime(2021, 2, 1))
